da3bf7bb6ad32db10990cf54fb0f3a2362f8d1e8
Artur Islamgulov <artur.islamgulov@gmail.com>
2019-02-25 16:52:51 +0300

    Create FizzBuzz.js
    Тестовое задание на JS.

---
+++ FizzBuzz.js
@@ -0,0 +1,20 @@
+const fizzBuzz = (begin, end) => {
+
+  if (begin <= end) {
+    for (let num = begin; num <= end; num++){
+      if (num % 3 == 0 && num % 5 == 0) {
+      console.log ('FizzBuzz');
+      }
+  else
+    if (num % 3 == 0) {
+      console.log ('Fizz');
+    }
+  else
+    if (num % 5 == 0) {
+      console.log ('Buzz');
+    }
+  else
+    console.log (num);
+    }
+  }
+}
